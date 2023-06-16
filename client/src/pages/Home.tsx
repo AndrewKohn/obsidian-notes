a70a6db25552f12ed33600c25f480d0810bc53b7
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-06-16 10:50:06 -0500

    Add folder structure and list of links

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,37 @@
-const Home = () => {
-  return <h1>HOME PAGE</h1>;
+interface Props {
+  links: {
+    folderPath: string[];
+    link: JSX.Element;
+  }[];
+}
+
+const Home = ({ links }: Props) => {
+  let prevPath: string[] = [''];
+
+  return (
+    <section>
+      <h1>HOME PAGE</h1>
+      <ul>
+        {links.map(
+          (link: { folderPath: string[]; link: JSX.Element }, key: number) => {
+            if (JSON.stringify(link.folderPath) !== JSON.stringify(prevPath)) {
+              prevPath = link.folderPath;
+
+              return (
+                <div key={key}>
+                  <hr />
+                  <p className="folder-name">{prevPath.join('/') + '/'}</p>
+                  <li key={key}>{link.link}</li>
+                </div>
+              );
+            }
+
+            return <li key={key}>{link.link}</li>;
+          }
+        )}
+      </ul>
+    </section>
+  );
 };
 
 export default Home;
