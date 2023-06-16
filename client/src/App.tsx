13056aa1a74e72996dae3fa67f7ed06f3e7b78f6
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-06-16 10:51:12 -0500

    add home component links parameter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import MarkdownPage from './components/MarkDownPage';
 import PageNotFound from './pages/PageNotFound';
 
+interface ObsidianPage {
+  fileName: string;
+  filePath: string;
+}
+
 const App: React.FC = () => {
   const [data, setData] = useState<string[] | undefined>(undefined);
-  const [obsidianPages, setObsidianPages] = useState<string[]>([]);
+  const [obsidianPages, setObsidianPages] = useState<ObsidianPage[]>([]);
 
   // Initial get.  Retrieves file paths.
   useEffect(() => {
@@ -25,18 +30,25 @@ const App: React.FC = () => {
   // Returns a filepath string & stores into array.
   useEffect(() => {
     if (data !== undefined) {
-      const updatedFilePath = data.map((filePath: string) => {
-        if (filePath.includes('\\')) {
-          return filePath.replace(/\\/g, '/');
-        }
-        return filePath;
+      const getObsidianFiles = data.map((path: string) => {
+        const filePath: string = updateFilePath(path);
+        const fileName: string = getFileName(filePath);
+
+        return { fileName: fileName, filePath: filePath };
       });
-      setObsidianPages(updatedFilePath);
+
+      setObsidianPages(getObsidianFiles);
     }
   }, [data]);
 
-  // Get file name from file path
-  const markdownRoutes = obsidianPages.map((filePath: string, key: number) => {
+  const updateFilePath = (filePath: string) => {
+    if (filePath.includes('\\')) {
+      return filePath.replace(/\\/g, '/');
+    }
+    return filePath;
+  };
+
+  const getFileName = (filePath: string) => {
     const reversedFilePath = filePath.split('').reverse().slice(3);
     let fileName = '';
 
@@ -47,20 +59,42 @@ const App: React.FC = () => {
 
     fileName = fileName.split('').reverse().join('');
 
-    return (
-      <Route
-        key={key}
-        path={`/${fileName}`}
-        element={<MarkdownPage key={filePath} filePath={filePath} />}
-      />
-    );
-  });
+    return fileName;
+  };
+
+  const mdRoutes = obsidianPages.map(
+    (obsidianPage: ObsidianPage, key: number) => {
+      return (
+        <Route
+          key={key}
+          path={`/${obsidianPage.fileName}`}
+          element={<MarkdownPage key={key} filePath={obsidianPage.filePath} />}
+        />
+      );
+    }
+  );
+
+  const mdLinks = obsidianPages.map(
+    (obsidianPage: ObsidianPage, key: number) => {
+      const filePath: string[] = obsidianPage.filePath.split('/');
+      const folderPath: string[] = filePath.splice(0, filePath.length - 1);
+
+      return {
+        folderPath: folderPath,
+        link: (
+          <Link key={key} to={obsidianPage.fileName}>
+            <p>{obsidianPage.fileName}</p>
+          </Link>
+        ),
+      };
+    }
+  );
 
   return (
     <main>
       <Routes>
-        <Route path="/" element={<Home />} />
-        {obsidianPages ? markdownRoutes.map((route: JSX.Element) => route) : ''}
+        <Route path="/" element={<Home links={mdLinks} />} />
+        {obsidianPages ? mdRoutes.map((route: JSX.Element) => route) : ''}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </main>
