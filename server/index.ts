6075c54d98c7a1a7dc3c25a7cbe22aeacbc3366c
AndrewKohn <87670203+itsRunic@users.noreply.github.com>
2023-07-07 04:52:53 -0500

    Add logs for server info

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,37 @@
 const express = require('express');
-import { Express } from 'express';
+import { Express, Request, Response, NextFunction } from 'express';
 const app: Express = express();
 const cors = require('cors');
 
-const PORT = 3000;
+const PORT = 3002;
 
 app.use(express.json());
 app.use(cors());
 
 app.use('/', require('./obsidianRoutes'));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const startTime = Date.now();
+  const currentDate = new Date();
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    timeZone: 'America/Chicago',
+    hour12: false,
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  };
+  const dateFormat = `${currentDate.getMonth()}/${currentDate.getDay()}/${currentDate.getFullYear()}`;
+  const centralTime = currentDate.toLocaleString('en-US', timeOptions);
+  const urlPath: string = req.baseUrl + req.originalUrl;
+
+  res.on('finish', () => {
+    const responseTime = Date.now() - startTime;
+    console.log(
+      `${dateFormat} ${centralTime}: ${responseTime}ms | [${req.method}] (${res.statusCode}) ${urlPath}`
+    );
+  });
+
+  next();
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
